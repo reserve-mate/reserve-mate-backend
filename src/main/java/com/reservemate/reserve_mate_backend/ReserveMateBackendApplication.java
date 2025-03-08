de131c7d855309787c97f87dfcd224cb11b7dcee
Anhyeonggwan <anhyeonggwan@gmail.com>
2025-03-08 20:25:19 +0900

    first commit

---
+++ src/main/java/com/reservemate/reserve_mate_backend/ReserveMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reservemate.reserve_mate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveMateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservemate/reserve_mate_backend/ReserveMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservemate.reserve_mate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveMateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
