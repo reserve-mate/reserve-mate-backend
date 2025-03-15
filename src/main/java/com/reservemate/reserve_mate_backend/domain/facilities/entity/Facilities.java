3f578bdc488574d0417be579d5c27f8c14abedba
yangnaeyoon <nyyang9921@gmail.com>
2025-03-15 18:21:26 +0900

    feat: entity 추가

---
+++ src/main/java/com/reservemate/reserve_mate_backend/domain/facilities/entity/Facilities.java
@@ -0,0 +1,29 @@
+package com.reservemate.reserve_mate_backend.domain.facilities.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Entity
+public class Facilities {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(length = 100)
+    private String name;
+    @Column(length = 300)
+    private String description;
+    @Column(length = 100)
+    private String address;
+    @Column(length = 100)
+    private String latitude;
+    @Column(length = 100)
+    private String longitude;
+    @Column(length = 11)
+    private String contactPhone;
+    @CreatedDate
+    @Column(updatable = false)
+    private String createdAt;
+    @LastModifiedDate
+    private String updatedAt;
+}
