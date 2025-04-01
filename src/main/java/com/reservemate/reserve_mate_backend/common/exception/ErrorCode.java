40735cdfc024e3f8415a67c4036d191b979de25b
Anhyeonggwan <anhyeonggwan@gmail.com>
2025-04-01 21:45:50 +0900

    매치 에러 코드 및 Util 추가

--- src/main/java/com/reservemate/reserve_mate_backend/common/exception/ErrorCode.java
+++ src/main/java/com/reservemate/reserve_mate_backend/common/exception/ErrorCode.java
@@ -3,6 +3,7 @@ package com.reservemate.reserve_mate_backend.common.exception;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.CONFLICT;
 
 import org.springframework.http.HttpStatus;
 
@@ -15,7 +16,10 @@ public enum ErrorCode {
 
     // common
     INVALID_INPUT_VALUE(BAD_REQUEST, "유효하지 않은 입력값입니다."), SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 오류"), USER_NOT_FOUND(
-        UNAUTHORIZED, "유저 정보를 찾을 수 없습니다"), UNAUTHORIZED_CODE(UNAUTHORIZED, "인증 정보가 올바르지 않습니다.");
+        UNAUTHORIZED, "유저 정보를 찾을 수 없습니다"), UNAUTHORIZED_CODE(UNAUTHORIZED, "인증 정보가 올바르지 않습니다.")
+
+    // 매치 관련 에러 처리
+    , EXIST_MATCH_ERROR(CONFLICT, "중복된 매치가 존재합니다."), EXIST_MATCH_TIME_ERROR(CONFLICT, "겹치는 시간대에 매치가 존재합니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/com/reservemate/reserve_mate_backend/common/util/Utils.java
+++ src/main/java/com/reservemate/reserve_mate_backend/common/util/Utils.java
@@ -1,12 +1,20 @@
 package com.reservemate.reserve_mate_backend.common.util;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
 public class Utils {
+
+    // 시간 겹치는지 확인
+    public static boolean isTimeConflict(LocalTime existStarTime, LocalTime existEndTime, LocalTime newStarTime,
+        LocalTime newEndTime) {
+        return !(newEndTime.isBefore(existStarTime) || newStarTime.isAfter(existEndTime));
+    }
 
     public static LocalDateTime localDateTimeFormat(LocalDateTime localDateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -19,6 +27,12 @@ public class Utils {
         String datePattern = localDate.format(formatter)
             + " " + localDate.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
         return datePattern;
+    }
+
+    // 오늘 요일 구하기
+    public static DayOfWeek getDayOfWeek() {
+        LocalDate toDay = LocalDate.now();
+        return toDay.getDayOfWeek();
     }
 
 }
