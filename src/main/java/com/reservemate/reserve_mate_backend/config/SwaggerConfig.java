98746a9531cbf949f5a80c009d123877318426c0
JWbase <jungwoo1002@gmail.com>
2025-04-16 23:30:22 +0900

    feat: swagger 개발서버 추가

--- src/main/java/com/reservemate/reserve_mate_backend/config/SwaggerConfig.java
+++ src/main/java/com/reservemate/reserve_mate_backend/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.reservemate.reserve_mate_backend.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -9,6 +11,11 @@ import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "https://api.sportmate.site", description = "개발 서버"),
+        @Server(url = "http://localhost:8080", description = "로컬 서버")
+    })
 @Configuration
 public class SwaggerConfig {
 
