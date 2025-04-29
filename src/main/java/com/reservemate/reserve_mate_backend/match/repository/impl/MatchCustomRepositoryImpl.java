e3882be5416256c2591b9ac1caee2d1dcffe59e5
Anhyeonggwan <anhyeonggwan@gmail.com>
2025-04-29 15:14:45 +0900

    매치 조회 로직 수정

--- src/main/java/com/reservemate/reserve_mate_backend/match/repository/impl/MatchCustomRepositoryImpl.java
+++ src/main/java/com/reservemate/reserve_mate_backend/match/repository/impl/MatchCustomRepositoryImpl.java
@@ -100,10 +100,13 @@ public class MatchCustomRepositoryImpl implements MatchCustomRepository {
             .from(match)
             .join(court).on(court.eq(match.court))
             .join(facility).on(facility.eq(court.facility))
-            .leftJoin(matchPlayer).on(matchPlayer.match.eq(match)).fetchJoin()
+            .leftJoin(matchPlayer).on(
+                matchPlayer.match.eq(match), matchPlayer.status.eq(PlayerStatus.READY)
+            )
+            .fetchJoin()
             .where(
                 matchDateEq(matchSearchDto.getMatchDate()), sportTypeEq(matchSearchDto.getSportType()), searchValueLike(
-                    matchSearchDto.getSearchValue()), matchPlayer.status.eq(PlayerStatus.READY)
+                    matchSearchDto.getSearchValue())
             )
             .groupBy(match.matchId)
             .orderBy(match.matchTime.asc())
