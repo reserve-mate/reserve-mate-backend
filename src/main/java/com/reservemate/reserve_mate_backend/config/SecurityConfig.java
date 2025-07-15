b8df642403785b55ff78978d184c2785eeffa90b
Anhyeonggwan <anhyeonggwan@gmail.com>
2025-07-15 19:12:10 +0900

    시간 지난 부분 제거

--- src/main/java/com/reservemate/reserve_mate_backend/config/SecurityConfig.java
+++ src/main/java/com/reservemate/reserve_mate_backend/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(
                 auth -> auth.requestMatchers("/login", "/", "/users/register", "/match/matcheDates", "/match/matches",
                     "/match/matches/*", "/reserve/reserveHours", "/review/*/reviews", "/review/*/reviewInfo",
-                    "/facility/list", "/facility/*/detail")
+                    "/facility/list", "/facility/*/detail", "/reserve/reserveHours")
                     .permitAll()
                     .requestMatchers(
                         "/mail/send/authCode",
